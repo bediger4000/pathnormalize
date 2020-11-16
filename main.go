0c2f6bb1d231d8f7d76ddb264e13703e831690a7
Bruce Ediger <bediger8@gmail.com>
2020-11-15 17:12:02 -0700

    Add commentary

--- main.go
+++ main.go
@@ -21,29 +21,38 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Path components: non-zero-length strings between '/' characters
+	// The non-zero-length part gets taken care of when examining
+	// path components inside the for-loop below.
 	components := strings.Split(os.Args[1], "/")
 
+	// Path components after normalizing
 	var normalizedPath []string
 
 	for _, component := range components {
+		if component == "" {
+			// Ignore zero-length components
+			continue
+		}
 		if component == "." {
+			// Path stays the same
 			continue
 		}
 		if component == ".." {
+			// pop a component off the normalizedPath stack,
+			// but don't go "before" the root of the path.
 			l := len(normalizedPath)
 			if l > 0 {
-				if len(normalizedPath[l-1]) > 0 {
-					normalizedPath = normalizedPath[:l-1]
-				}
+				normalizedPath = normalizedPath[:l-1]
 			}
 			continue
 		}
-
-		if len(component) > 0 {
-			normalizedPath = append(normalizedPath, component)
-		}
+		normalizedPath = append(normalizedPath, component)
 	}
 
+	// The above algorithm doesn't do leading and trailing
+	// slashes. Trailing slashes don't appear on a zero-length
+	// normalized path.
 	path := strings.Join(normalizedPath, "/")
 	if len(path) > 0 {
 		fmt.Printf("/%s/\n", path)
